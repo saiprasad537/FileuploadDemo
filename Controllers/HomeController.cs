178cac76c39e4a879a3df62b585cde741fdd0f07
Saiprasad Chandupatla <spchandu537@gmail.com>
2018-06-10 22:32:34 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileuploadDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile()
+        {
+            try
+            {
+                HttpPostedFileBase hpf = HttpContext.Request.Files["file"] as HttpPostedFileBase;
+                string tag = HttpContext.Request.Params["tags"];// The same param name that you put in extrahtml if you have some.
+                string category = HttpContext.Request.Params["category"];
+
+                DirectoryInfo di = Directory.CreateDirectory(Server.MapPath("~/Tmp/Files"));// If you don't have the folder yet, you need to create.
+                string sentFileName = Path.GetFileName(hpf.FileName); //it can be just a file name or a user local path! it depends on the used browser. So we need to ensure that this var will contain just the file name.
+                string savedFileName = Path.Combine(di.FullName, sentFileName);
+                hpf.SaveAs(savedFileName);
+                        
+                var msg = new { msg = "File Uploaded", filename = hpf.FileName, url= savedFileName };
+                return Json(msg,"text/x-json");
+            }
+            catch (Exception e)
+            {
+                //If you want this working with a custom error you need to change in file jquery.uploadfile.js, the name of 
+                //variable customErrorKeyStr in line 85, from jquery-upload-file-error to jquery_upload_file_error 
+                var msg = new { jquery_upload_file_error = e.Message };
+                return Json(msg, "text/x-json");
+            }
+        }
+      
+        public ActionResult DownloadFile(string url)
+        {
+            return File(url,"");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(string url)
+        {
+            try
+            {
+                System.IO.File.Delete(url);
+                var msg = new { msg = "File Deleted!" };
+                return Json(msg);
+            }
+            catch (Exception e)
+            {
+                //If you want this working with a custom error you need to change the name of 
+                //variable customErrorKeyStr in line 85, from jquery-upload-file-error to jquery_upload_file_error 
+                var msg = new { jquery_upload_file_error = e.Message };
+                return Json(msg);
+            }
+        }
+        //
+        // GET: /Home/
+
+       
+    }
+}
---
+++ Views/Home/Index.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Index</title>
+    <link href="https://rawgithub.com/hayageek/jquery-upload-file/master/css/uploadfile.css" rel="stylesheet" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="../../Content/jquery.uploadfile.js"></script>
+</head>
+<body>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div id="fileuploader">Upload</div>
+        </div>
+    </div>
+    <div id="extrabutton" class="ajax-file-upload-green">Start Upload</div>
+
+    <script>
+        //This example show how to use upload plugin with Asp.Net C# MVC
+        //The plugin use an extraHTML, download and delete buttons
+        $(document).ready(function () {
+            var extraObj = $("#fileuploader").uploadFile({
+                url: "../Home/UploadFile",
+                statusBarWidth: 'auto',
+                dragdropWidth: 'auto',
+                showDelete: true,
+                showDownload: true,
+                autoSubmit: false,
+                showProgress: true,
+                extraHTML: function () {
+                    var html = "<div><b>File Tags:</b><input type='text' name='tags' value='' /> <br/>";
+                    html += "<b>Category</b>:<select name='category'><option value='1'>ONE</option><option value='2'>TWO</option></select>";
+                    html += "</div>";
+                    return html;
+                },
+                deleteCallback: function (data, pd) {
+                    $.post("../Home/DeleteFile?url=" + data.url,
+                        function (resp, textStatus, jqXHR) {
+                            //Show Message	
+                            console.log(resp, textStatus);
+                            alert(resp.msg);
+                            pd.statusbar.hide(); //You choice.
+                        });
+                },
+                downloadCallback: function (json, pd) {
+                    console.log(pd);
+                    window.open('../Home/DownloadFile?url=' + json.url, "_blank");
+                }
+            });
+            $("#extrabutton").click(function () {
+                extraObj.startUpload();
+            });
+        });
+    </script>
+</body>
+</html>
